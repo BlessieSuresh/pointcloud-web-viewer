87983c3d11e2c9563af2bf5dc95595cbe1861110
Florian <florian@bildspur.ch>
2019-08-19 14:19:44 +0200

    combined uglify and browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,39 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
-var watchify = require('watchify');
 var tsify = require('tsify');
-var fancy_log = require('fancy-log');
-
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var buffer = require('vinyl-buffer');
-
 var paths = {
     pages: ['src/*.html']
 };
-
-var watchedBrowserify = watchify(browserify({
-    basedir: '.',
-    debug: true,
-    entries: ['src/main.ts'],
-    cache: {},
-    packageCache: {}
-})
-  .plugin(tsify)
-  .transform('babelify', {
-        presets: ['env'],
-        extensions: ['.ts']
-    }));
 
 gulp.task('copy-html', function () {
     return gulp.src(paths.pages)
         .pipe(gulp.dest('dist'));
 });
 
-function bundle() {
-    return watchedBrowserify
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(buffer())
-	    .pipe(sourcemaps.init({loadMaps: true}))
-	    .pipe(uglify())
-	    .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('dist'));
-}
+gulp.task('build-js', function () {
+    return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/main.ts'],
+        cache: {},
+        packageCache: {}
+    })
+    .plugin(tsify)
+    .transform('babelify', {
+        presets: ['env'],
+        extensions: ['.ts']
+    })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'));
+});
 
-gulp.task('default', gulp.series(gulp.parallel('copy-html'), bundle));
-watchedBrowserify.on('update', bundle);
-watchedBrowserify.on('log', fancy_log);
+gulp.task('default', gulp.series(gulp.parallel('build-js', 'copy-html')));
