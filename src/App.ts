c7c9d266cdb076d8b909953198a466bfbba9e925
Florian <florian@bildspur.ch>
2019-08-19 18:50:44 +0200

    removed unused import

--- src/App.ts
+++ src/App.ts
@@ -7,8 +7,6 @@ import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader";
 import {BufferGeometry} from "three";
 
 let Stats = require('three/examples/jsm/libs/stats.module.js');
-//import {Stats} from "three/examples/jsm/libs/stats.module.js"
-
 
 export class App {
 
