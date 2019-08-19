798ce7f7c93fa1af97e044294dae836eec03aa55
Florian <florian@bildspur.ch>
2019-08-19 14:46:51 +0200

    implemented gulp with assets

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,40 @@ var tsify = require('tsify');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var buffer = require('vinyl-buffer');
+var sass = require('gulp-sass');
+
 var paths = {
-    pages: ['src/*.html']
+    pages: ['src/*.html'],
+    libs: [
+        'libs/**/*.*'
+    ],
+    watch: [
+        './src/*.ts',
+        './src/**/*.ts',
+        './src/*.html',
+        './scss/*.scss'
+    ],
+    assets: [
+        'assets/**/*.*'
+    ]
 };
 
-gulp.task('copy-html', function () {
+gulp.task('html', function () {
     return gulp.src(paths.pages)
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-js', function () {
+gulp.task('libs', function () {
+    return gulp.src(paths.libs)
+        .pipe(gulp.dest('./dist/libs'));
+});
+
+gulp.task('assets', function () {
+    return gulp.src(paths.assets)
+        .pipe(gulp.dest('./dist/assets'));
+});
+
+gulp.task('js', function () {
     return browserify({
         basedir: '.',
         debug: true,
@@ -22,18 +46,31 @@ gulp.task('build-js', function () {
         cache: {},
         packageCache: {}
     })
-    .plugin(tsify)
-    .transform('babelify', {
-        presets: ['env'],
-        extensions: ['.ts']
-    })
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(uglify())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dist'));
+        .plugin(tsify)
+       	.transform('babelify', {
+        	presets: ['env'],
+        	extensions: ['.ts']
+    	})
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+	    .pipe(sourcemaps.init({loadMaps: true}))
+	    .pipe(uglify())
+	    .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', gulp.series(gulp.parallel('build-js', 'copy-html')));
+gulp.task('watch', function() {
+    gulp.watch(paths.watch, gulp.series('default'));
+});
+
+gulp.task('sass', function(done) {
+    gulp.src('./scss/main.scss')
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+        .pipe(gulp.dest('./dist'))
+        .on('end', done);
+});
+
+gulp.task('default', gulp.series(gulp.parallel('html', 'libs', 'sass', 'assets', 'js')));
