c36f5365f1f1cfbb87ce32d589fb933423d76441
Florian <florian@bildspur.ch>
2019-08-19 13:55:51 +0200

    added browerify

--- src/App.ts
+++ src/App.ts
@@ -2,5 +2,8 @@
 export class App {
 	public hello(compiler: string) {
     	console.log(`Hello from ${compiler}`);
+
+    	const elt = document.getElementById("greeting");
+    	elt.innerText = `Hello from ${compiler}`;
 	}
 }
